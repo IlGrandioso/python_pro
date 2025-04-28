fe77c809efe944816fe7cc8853737e7800460ca0
IlGrandioso <serbanchristian3@gmail.com>
2025-04-28 17:13:59 +0200

    Create main.py
    FFIIRRSSTT PPRROOGGRRAAMMMM

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+# Sorprendi i tuoi compagni!
+lnm = ["Pugilato", "Pasta", "Matematica", "Rumeno", "Cubi di Rubik"]
+print(random.choice(lnm))
+ite = []
+d = int(input('Quanti fatti vuoi mettere nella lista'))
+for i in range(d):
+    s = input('Che fatto vuoi mettere')
+    ite.append(s)
+print(random.choice(ite))
